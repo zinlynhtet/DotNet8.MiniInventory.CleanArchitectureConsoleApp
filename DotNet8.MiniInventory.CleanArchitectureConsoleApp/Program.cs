71f83226d67034c1b280cc4c58c6dad0c17758fa
Zin Linn Htet <mackk5504@gmail.com>
2024-05-21 13:59:12 +0630

    Add Config

--- DotNet8.MiniInventory.CleanArchitectureConsoleApp/Program.cs
+++ DotNet8.MiniInventory.CleanArchitectureConsoleApp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var repository = new InventoryRepository();
+var useCases = new InventoryUseCases(repository);
+var consoleInterface = new InventoryConsoleInterface(useCases);
+
+while (true)
+{
+    Console.WriteLine("1. Add Item");
+    Console.WriteLine("2. List Items");
+    Console.WriteLine("3. Update Item");
+    Console.WriteLine("4. Delete Item");
+    Console.WriteLine("5. Exit");
+    Console.Write("Select an option: ");
+
+    var option = int.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1:
+            consoleInterface.AddItem();
+            break;
+        case 2:
+            consoleInterface.ListItems();
+            break;
+        case 3:
+            consoleInterface.UpdateItem();
+            break;
+        case 4:
+            consoleInterface.DeleteItem();
+            break;
+        case 5:
+            return;
+        default:
+            Console.WriteLine("Invalid option. Try again.");
+            break;
+    }
+}
