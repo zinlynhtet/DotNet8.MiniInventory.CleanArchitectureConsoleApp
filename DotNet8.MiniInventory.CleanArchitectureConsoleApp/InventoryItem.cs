5eb6a816eb7ca7a59a20039b96dc3a19025e6185
Zin Linn Htet <mackk5504@gmail.com>
2024-05-21 14:12:43 +0630

    Complete Data Model

---
+++ DotNet8.MiniInventory.CleanArchitectureConsoleApp/InventoryItem.cs
@@ -0,0 +1,7 @@
+public class InventoryItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
