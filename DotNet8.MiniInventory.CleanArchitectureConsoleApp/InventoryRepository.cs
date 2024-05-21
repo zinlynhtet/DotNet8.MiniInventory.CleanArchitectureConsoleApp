769a4bfa096e8a49e44c487ff015474878592329
Zin Linn Htet <mackk5504@gmail.com>
2024-05-21 14:12:58 +0630

    Complete Inventory Repository

---
+++ DotNet8.MiniInventory.CleanArchitectureConsoleApp/InventoryRepository.cs
@@ -0,0 +1,41 @@
+public class InventoryRepository : IInventoryRepository
+{
+    private readonly List<InventoryItem> _items = new();
+    private int _nextId = 1;
+
+    public void AddItem(InventoryItem item)
+    {
+        item.Id = _nextId++;
+        _items.Add(item);
+    }
+
+    public List<InventoryItem> GetItems()
+    {
+        return _items;
+    }
+
+    public InventoryItem GetItemById(int id)
+    {
+        return _items.FirstOrDefault(i => i.Id == id);
+    }
+
+    public void UpdateItem(InventoryItem item)
+    {
+        var existingItem = _items.FirstOrDefault(i => i.Id == item.Id);
+        if (existingItem != null)
+        {
+            existingItem.Name = item.Name;
+            existingItem.Quantity = item.Quantity;
+            existingItem.Price = item.Price;
+        }
+    }
+
+    public void DeleteItem(int id)
+    {
+        var item = _items.FirstOrDefault(i => i.Id == id);
+        if (item != null)
+        {
+            _items.Remove(item);
+        }
+    }
+}
